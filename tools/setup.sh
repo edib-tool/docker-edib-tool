c63dc5beec5150cb1527c3a4050372a975eaf885
Christoph Grabo <asaaki@mannaz.cc>
2016-09-20 15:33:44 +0200

    Build ExStripZip from source

--- tools/setup.sh
+++ tools/setup.sh
@@ -3,10 +3,15 @@ set -e
 
 TOOLS_DIR="/build/tools"
 ESZ="ex_strip_zip"
-ESZ_VER="0.0.2"
 ESZ_HOME="https://github.com/ntrepid8/$ESZ"
-ESZ_DL_URL="$ESZ_HOME/releases/download/v$ESZ_VER/$ESZ"
 
 echo "Fetching ExStripZip ($ESZ_HOME) ..."
-curl -sSL $ESZ_DL_URL -o $TOOLS_DIR/$ESZ
-chmod +x $TOOLS_DIR/$ESZ
+ESZ_BUILD_DIR=$(mktemp -d)
+cd $ESZ_BUILD_DIR
+  git clone $ESZ_HOME
+  cd $ESZ
+    MIX_ENV=prod mix escript.build
+    cp $ESZ $TOOLS_DIR/$ESZ
+    chmod +x $TOOLS_DIR/$ESZ
+cd ~
+rm -rf $ESZ_BUILD_DIR
