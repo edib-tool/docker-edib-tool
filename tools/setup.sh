243849983e47440e73d444a02db9a3ede7697adf
Christoph Grabo <asaaki@mannaz.cc>
2017-01-15 17:37:48 +0100

    Fix ESZ installation

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-set -e
+set -ex
 
 TOOLS_DIR="/build/tools"
 ESZ="ex_strip_zip"
-ESZ_HOME="https://github.com/ntrepid8/$ESZ"
+ESZ_ZIP_BRANCH="master"
+ESZ_HOME_ZIP="https://github.com/ntrepid8/$ESZ/archive/$ESZ_ZIP_BRANCH.zip"
 
 echo "Fetching ExStripZip ($ESZ_HOME) ..."
 ESZ_BUILD_DIR=$(mktemp -d)
 cd $ESZ_BUILD_DIR
-  git clone $ESZ_HOME
-  cd $ESZ
+  curl -sSL $ESZ_HOME_ZIP -o $ESZ.zip
+  unzip $ESZ.zip
+  cd $ESZ-$ESZ_ZIP_BRANCH
     MIX_ENV=prod mix escript.build
     cp $ESZ $TOOLS_DIR/$ESZ
     chmod +x $TOOLS_DIR/$ESZ
