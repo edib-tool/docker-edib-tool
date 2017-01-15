842a7dfde82072429b4ace4764e947774fb1ff2c
Christoph Grabo <asaaki@mannaz.cc>
2017-01-15 18:48:57 +0100

    Fix warnings for keyword dicts

--- tools/app_info.exs
+++ tools/app_info.exs
@@ -1,8 +1,8 @@
 defmodule AppInfo do
-  def name, do: config() |> Dict.get(:app)
-  def version, do: config() |> Dict.get(:version)
-  def distillery, do: deps() |> Dict.has_key?(:distillery)
-  def phoenix, do: deps() |> Dict.has_key?(:phoenix)
+  def name, do: config() |> Keyword.get(:app)
+  def version, do: config() |> Keyword.get(:version)
+  def distillery, do: deps() |> Keyword.has_key?(:distillery)
+  def phoenix, do: deps() |> Keyword.has_key?(:phoenix)
   defp config, do: Mix.Project.config()
-  defp deps, do: config() |> Dict.get(:deps)
+  defp deps, do: config() |> Keyword.get(:deps)
 end
