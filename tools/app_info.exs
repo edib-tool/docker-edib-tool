ef85f230f2189ec11c1b451440b5c880f91321c9
Thomas Depierre <thomas.depierre@ascential.com>
2017-07-31 11:35:08 +0100

    makes versions match

--- tools/app_info.exs
+++ tools/app_info.exs
@@ -3,11 +3,12 @@ defmodule AppInfo do
   def version, do: config() |> Keyword.get(:version)
   def distillery, do: deps() |> Keyword.has_key?(:distillery)
   def phoenix, do: deps() |> Keyword.has_key?(:phoenix)
-  def phoenix_1_3 do
+  def phoenix_1_3() do
    deps()
    |> Keyword.get(:phoenix)
-   |> elem(1)
-   |> Version.match?(">= 1.3-a")
+   |> String.split()
+   |> Enum.at(1)
+   |> Version.match?(">= 1.3.0-a")
  end
 
   defp config, do: Mix.Project.config()
