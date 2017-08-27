c846520bead97861ba89d57c5b3deacb065da08d
Christoph Grabo <asaaki@mannaz.cc>
2017-08-27 15:22:48 +0200

    Add bash to rootfs image
    Addresses issue #15 and hopefully fixes it as well.
    This is a workaround for the bash requirement in distillery.

--- edib/make-tarball.mk
+++ edib/make-tarball.mk
@@ -5,10 +5,12 @@ include edib/shared.mk
 ROOTFS              = $(STAGE_DIR)/rootfs
 ROOTFS_BIN          = $(ROOTFS)/bin
 ROOTFS_SH           = $(ROOTFS_BIN)/sh
+ROOTFS_BASH         = $(ROOTFS_BIN)/bash
 ROOTFS_APP          = $(ROOTFS)/app
 ROOTFS_APP_BIN      = $(ROOTFS_APP)/bin
 BUSYBOX             = /bin/busybox
 SYSTEM_FILES        = $(shell $(TOOLS_DIR)/libdeps)
+SYSTEM_BASH         = /bin/bash
 SOURCE_FILES        = $(SYSTEM_FILES) $(BUSYBOX)
 ROOTFS_SYSTEM_FILES = $(SOURCE_FILES:%=$(ROOTFS)%)
 
@@ -39,7 +41,7 @@ tarball: $(NAMED_TARBALL) $(LINKED_TARBALL)
 $(LINKED_TARBALL): $(NAMED_TARBALL)
 	cd $(@D) && ln -sf $(TIMESTAMP)/$(@F) $(@F)
 
-$(NAMED_TARBALL): $(ROOTFS_SYSTEM_FILES) $(ROOTFS_SH) $(ROOTFS_APP_BIN)
+$(NAMED_TARBALL): $(ROOTFS_SYSTEM_FILES) $(ROOTFS_SH) $(ROOTFS_BASH) $(ROOTFS_APP_BIN)
 	mkdir -p $(@D) && cd $(ROOTFS) && tar -czvf $@ .
 
 $(ROOTFS_SYSTEM_FILES): $(ROOTFS)%: $(ROOTFS)
@@ -47,6 +49,9 @@ $(ROOTFS_SYSTEM_FILES): $(ROOTFS)%: $(ROOTFS)
 
 $(ROOTFS_SH): $(ROOTFS_BIN)
 	$(BUSYBOX) --install -s $(ROOTFS_BIN)
+
+$(ROOTFS_BASH): $(ROOTFS_BIN) $(SYSTEM_BASH)
+	cp $(word 2,$^) $@
 
 $(ROOTFS_BIN): $(ROOTFS)
 	mkdir -p $@
